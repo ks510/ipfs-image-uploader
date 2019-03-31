7397260a252dcf4714de60148951b249265528f9
Karen Suen <karen@Karens-MacBook-Air.local>
2019-04-01 00:37:14 +0100

    added IPFS upload & stored hash in contract

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
+import ipfs from './ipfs';
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  constructor(props) {
+    super(props);
+    this.state = {
+      storageValue: 0,
+      web3: null,
+      accounts: null,
+      contract: null,
+      buffer: null,
+      ipfsHash: ''
+    };
+  }
+
 
   componentDidMount = async () => {
     try {
@@ -25,7 +37,7 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance }, this.retrieveFile);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,18 +47,54 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
+  retrieveFile = async () => {
     const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
     // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
+    const ipfsHash = await contract.methods.get().call();
     // Update state with the result.
-    this.setState({ storageValue: response });
+    this.setState({ ipfsHash: ipfsHash });
   };
+
+
+  // uploads the buffer file stored in state to IPFS using ipfs API
+  // store the IPFS hash in contract
+  onSubmit = async (event) => {
+    event.preventDefault(); // prevents browser from refreshing or getting a new page on click
+    console.log("on submit...");
+
+    // post file to IPFS, get the IPFS hash and store it in contract
+    try {
+      let results = await ipfs.add(this.state.buffer);
+      let ipfsHash = results[0].hash;
+
+      // store IPFS hash in contract
+      const { accounts, contract } = this.state;
+      await contract.methods.set(ipfsHash).send({ from: accounts[0] });
+
+      this.setState({ ipfsHash: ipfsHash });
+
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  // this method is called whenever a file is uploaded
+  // gets uploaded file and converts it to appropriate format for IPFS
+  // stores the file in this component's state
+  captureFile = (event) => {
+    event.preventDefault();
+
+    const file = event.target.files[0]; // access file from user input
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file); // convert file to array for buffer
+    // after reader finishes, initialise buffer and store in component state
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) });
+
+      console.log('buffer', this.state.buffer); // console should log uint8array...
+    }
+  }
 
   render() {
     if (!this.state.web3) {
@@ -54,17 +102,14 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>Your Image</h1>
+        <p>This image is stored on IPFS & Ethereum blockchain!</p>
+        <img src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt="" />
+        <h2>Upload Image</h2>
+        <form onSubmit={this.onSubmit}>
+          <input type='file' onChange={this.captureFile}/>
+          <input type='submit' />
+        </form>
       </div>
     );
   }
